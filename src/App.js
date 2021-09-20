0274a195668fc06bf9dbe285853f4ebae6d7059b
JahangeerAnsari <jahangeeransari194@gmail.com>
2021-09-20 20:20:18 +0530

    page not found added

--- src/App.js
+++ src/App.js
@@ -1,46 +1,35 @@
-
-import './App.css';
-import { Route, Switch } from 'react-router-dom'
-import Home from './container/Home';
-import Signup from './container/Signup';
-import Signin from './container/Signin';
-import PrivateRotue from './components/HOC/PrivateRoute';
-import { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { isUserLogin } from './actions/auth.action';
-import UserPage from './container/UserPage';
-import AssesmentPage from './container/AssesmentPage';
-import QuestionPage from './container/QuestionPage';
-import { fetchedAllAssesments } from './actions/assesment.action';
-import ResultPage from './container/ResultPage';
-
+import "./App.css";
+import { Route, Switch } from "react-router-dom";
+import Home from "./container/Home";
+import Signup from "./container/Signup";
+import Signin from "./container/Signin";
+import PrivateRotue from "./components/HOC/PrivateRoute";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { isUserLogin } from "./actions/auth.action";
+import UserPage from "./container/UserPage";
+import AssesmentPage from "./container/AssesmentPage";
+import QuestionPage from "./container/QuestionPage";
+import { fetchedAllAssesments } from "./actions/assesment.action";
+import ResultPage from "./container/ResultPage";
+import Page404 from "./container/Page404";
 
 function App() {
   const dispatch = useDispatch();
-  const auth = useSelector(state => state.auth);
-  
-  useEffect(() => {
+  const auth = useSelector((state) => state.auth);
 
-    
+  useEffect(() => {
     if (!auth.authenticate) {
       dispatch(isUserLogin());
     }
 
-
-    if(auth.authenticate){
+    if (auth.authenticate) {
       dispatch(fetchedAllAssesments());
     }
-    
-     
-    
-    
-  
   }, [auth.authenticate]);
-
 
   return (
     <div className="App">
-
       <Switch>
         <PrivateRotue exact path="/" component={Home} />
         <PrivateRotue path="/user" component={UserPage} />
@@ -49,8 +38,8 @@ function App() {
         <PrivateRotue path="/result" component={ResultPage} />
         <Route path="/signup" component={Signup} />
         <Route path="/signin" component={Signin} />
+        <Route path="*" component={Page404} />
       </Switch>
-
     </div>
   );
 }
---
+++ src/container/Page404/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Page404 = () => {
+  return <div>Oops! resourse not found.</div>;
+};
+
+export default Page404;
